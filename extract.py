f1676bce3fb57bd923fc5f68541c587a4c3bc18f
haris55555 <108285896+haris55555@users.noreply.github.com>
2026-06-13 04:22:06 +0530

    fix ectract.py upload fail

--- extract.py
+++ extract.py
@@ -1,18 +1,1 @@
-import sys
-import json
-import pdfplumber
-
-def extract_tables(pdf_path):
-  pdf = pdfplumber.open(pdf_path)
-  all_tables = []
-  for page in pdf.pages:
-    tables = page.extract_tables()
-    for table in tables:
-      all_tables.append(table)
-  pdf.close()
-  return all_tables
-
-pdf_path = sys.argv[1]
-result = extract_tables(pdf_path)
-print(json.dumps(result))
-
+import sys,json,pdfplumber; pdf=pdfplumber.open(sys.argv[1]); tables=[t for p in pdf.pages for t in p.extract_tables()]; print(json.dumps(tables))
